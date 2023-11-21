e0db8a981b9f39a62ac22bed7aa87a18044a2caa
xingyi <973294773@qq.com>
2023-11-21 13:52:55 +0800

    [2]手动模式+自动模式

--- User/main.c
+++ User/main.c
@@ -21,36 +21,39 @@ int main(void)
 	OLED_ShowString(1, 1, "State:");
 	OLED_ShowString(2, 1, "Switch:");
 	OLED_ShowString(3, 1, "Time:00:00:00");
+	OLED_ShowString(4, 1, "Ls:");
+	OLED_ShowString(4, 8, "Is:");
+
 
 	while (1)
 	{
-		if (key2_State == 0) //手动模式
+		if (key2_State == 1)
 		{
-			OLED_ShowNum(1, 7, key2_State, 1);
-			while (key2_State == 0)
+			if (Sensor_Value[1] >= 3000 && Sensor_Value[0] <= 3000)
 			{
-				if (key1_Switch == 0)
-				{
-					OLED_ShowString(2, 8, "OFF");
-					PWM_SetBrightness(0);
-				}
-				else if (key1_Switch == 1)
-				{
-					OLED_ShowString(2, 8, "ON ");
-					PWM_SetBrightness(100);
-				}
-        		OLED_ShowNum(3, 6, hour, 2);
-        		OLED_ShowNum(3, 9, min, 2);
-        		OLED_ShowNum(3, 12, sec, 2);
+				key1_Switch = 1;
+			}
+			else 
+			{
+				key1_Switch = 0;
 			}
 		}
-		else if (key2_State == 1) //自动模式
+		if (key1_Switch == 0)
 		{
-			OLED_ShowNum(1, 7, key2_State, 1);
-			while (key2_State == 1)
-			{
+			OLED_ShowString(2, 8, "OFF");
+			PWM_SetBrightness(0);
+		}
+		else if (key1_Switch == 1)
+		{
+			OLED_ShowString(2, 8, "ON ");
+			PWM_SetBrightness(100);
+		}
 
-			}
-		}
+		OLED_ShowNum(1, 7, key2_State, 1);
+		OLED_ShowNum(4, 4, Sensor_Value[1], 4);
+		OLED_ShowNum(4, 11, Sensor_Value[0], 4);
+		OLED_ShowNum(3, 6, hour, 2);
+		OLED_ShowNum(3, 9, min, 2);
+		OLED_ShowNum(3, 12, sec, 2);
 	}
 }
